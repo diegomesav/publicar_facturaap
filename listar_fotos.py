7f1fb76a8a8048d9ef87a813a63db4c40baa30ad
diegomesav <74283585+diegomesav@users.noreply.github.com>
2024-12-03 20:42:08 -0500

    renombrar fotos

--- listar_fotos.py
+++ listar_fotos.py
@@ -2,8 +2,28 @@
 import os
 import pandas as pd
 
+
+
 # Ruta de la carpeta que deseas listar
 ruta_carpeta = "xiomara"
+
+
+
+# Recorrer todos los archivos en la carpeta
+for archivo in os.listdir(ruta_carpeta):
+    # Verificar si el archivo comienza con "_"
+    if archivo.startswith("_"):
+        # Crear el nuevo nombre eliminando el primer carácter
+        nuevo_nombre = archivo[1:]
+        
+        # Generar las rutas completas (original y nueva)
+        ruta_original = os.path.join(ruta_carpeta, archivo)
+        ruta_nueva = os.path.join(ruta_carpeta, nuevo_nombre)
+        
+        # Renombrar el archivo
+        os.rename(ruta_original, ruta_nueva)
+        print(f"Renombrado: {archivo} -> {nuevo_nombre}")
+
 
 # Listar los archivos en la carpeta
 archivos = os.listdir(ruta_carpeta)
